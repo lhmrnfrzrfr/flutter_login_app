077ede868a2d96734e690a12e69ba7e9dc7b412c
unknown <ilhamriana3@gmail.com>
2019-03-17 16:01:10 +0700

    komit ketiga

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'auth.dart';
 
 class LoginPage extends StatefulWidget {
-  LoginPage({this.auth});
+  LoginPage({this.auth, this.onSignedIn});
   final BaseAuth auth;
+  final VoidCallback onSignedIn;
   
   @override
   State<StatefulWidget> createState() => new _LoginPageState();
@@ -41,6 +42,7 @@ class _LoginPageState extends State<LoginPage> {
         String userId = await widget.auth.createUserWithEmailAndPassword(_email, _password);
         print('Registered user: $userId');
         }
+        widget.onSignedIn();
       }
       catch (e) {
         print('Error: $e');
